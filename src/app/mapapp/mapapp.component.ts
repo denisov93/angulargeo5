cc99351ed859a45c3c834008bd02ae701c7ad04f
denisov93 <a.denisov@campus.fct.unl.pt>
2020-04-28 12:22:25 +0100

    draggableMap

--- src/app/mapapp/mapapp.component.ts
+++ src/app/mapapp/mapapp.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { AgmCoreModule } from '@agm/core';
+import { Component, OnInit, ViewChild, ElementRef, NgZone  } from '@angular/core';
+import { AgmCoreModule, MapsAPILoader, MouseEvent } from '@agm/core';
 import { Url } from 'url';
 import { RequestService } from '../services/RequestService';
 
@@ -9,22 +9,45 @@ import { RequestService } from '../services/RequestService';
   styleUrls: ['./mapapp.component.css']
 })
 export class MapappComponent implements OnInit {
-    lat: number = 38.661076;
-    lng: number = -9.205908;
+    
+  latitude: number = 38.661076;
+  longitude: number = -9.205908;
+  zoom:number;
+    
+  
+  imagesRandom:randomImages[];
 
-    imagesRandom:randomImages[];
-
-    constructor(private req:RequestService){    }
+    constructor(private req:RequestService, private mapsAPILoader: MapsAPILoader,
+      private ngZone: NgZone){    }
   
   
   ngOnInit(): void {
-    this.req.getTodos().subscribe(reqw => {
+    this.setCurrentLocation();
+
+ /*   this.req.getTodos().subscribe(reqw => {
       this.imagesRandom = reqw;
       console.log(reqw);
     });
-
+   */ 
   }
 
+// Get Current Location Coordinates
+private setCurrentLocation() {
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+      this.zoom = 15;
+    });
+  }
+}
+
+markerDragEnd($event: MouseEvent) {
+  console.log($event);
+  this.latitude = $event.coords.lat;
+  this.longitude = $event.coords.lng;
+  
+}
 
 }
 export class randomImages {
