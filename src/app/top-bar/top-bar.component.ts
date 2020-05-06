08f29c3a3eef45b5b56c43a707d9ecb4b8646a56
Sara Trafaria <sara.afonsoft@gmail.com>
2020-05-06 19:00:00 +0100

    Top-bar updated

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,30 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { RequestService } from '../services/RequestService';
-import { Router } from '@angular/router';
-import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
-export class TopBarComponent implements OnInit {
-  isLogOutError: boolean = false; 
+export class TopBarComponent implements OnInit { 
  
-  constructor(private request:RequestService,private router : Router) {   }
+  constructor() {   }
   
   ngOnInit(): void {
-  }
-
-  logOut(){
-    this.request.logOutUser(localStorage.getItem('tokenID')).subscribe((data : any)=>{
-      localStorage.removeItem('tokenID');  
-      this.router.navigate(['/home']);
-    },
-    (err : HttpErrorResponse)=>{
-      this.isLogOutError = true;
-    });
-
   }
 
 }
