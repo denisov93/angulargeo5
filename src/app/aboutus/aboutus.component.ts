21fb20f4da2b3d68a02054f07fb82983e23bde47
Sara Trafaria <sara.afonsoft@gmail.com>
2020-05-13 20:01:47 +0100

    aboutus updated

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,53 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Observer } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-aboutus',
   templateUrl: './aboutus.component.html',
   styleUrls: ['./aboutus.component.css']
 })
+
 export class AboutusComponent implements OnInit {
-  base64Image: any;
-  constructor() { }
 
-  ngOnInit() {
-    let imageUrl = '';
-
-    this.getBase64ImageFromURL(imageUrl).subscribe(base64data => {
-      console.log(base64data);
-      this.base64Image = 'data:image/jpg;base64,' + base64data;
-    });
+  constructor( public translate: TranslateService,) { 
+    translate.addLangs(['pt','en']); 
+    translate.setDefaultLang(localStorage.getItem('language'));
   }
-
-  getBase64ImageFromURL(url: string) {
-    return Observable.create((observer: Observer<string>) => {
-      let img = new Image();
-      img.crossOrigin = 'Anonymous';
-      img.src = url;
-      if (!img.complete) {
-        img.onload = () => {
-          observer.next(this.getBase64Image(img));
-          observer.complete();
-        };
-        img.onerror = (err) => {
-          observer.error(err);
-        };
-      } else {
-        observer.next(this.getBase64Image(img));
-        observer.complete();
-      }
-    });
-  }
-
-  getBase64Image(img: HTMLImageElement) {
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-    var dataURL = canvas.toDataURL("image/png");
-    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-  }
-
+  ngOnInit() { }
 
 }
