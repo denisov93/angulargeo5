0b3e409e287e1a03b7a3b7c4cb682cde8f81095f
Sara Trafaria <sara.afonsoft@gmail.com>
2020-05-15 11:43:11 +0100

    usermodule

---
+++ src/app/models/User.ts
@@ -0,0 +1,12 @@
+export class User{
+    user_username: String
+    user_name: String
+    user_email: String
+    user_role:String
+    user_creation_time: String
+    user_last_update_time: String
+    user_street: String
+    user_place: String
+    user_country: String
+    active_account: Boolean
+}
