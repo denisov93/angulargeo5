f76e83a41e8f4018c85b0ea1bc9ae49e8700b53f
Sara Trafaria <sara.afonsoft@gmail.com>
2020-05-15 11:34:16 +0100

    Person update

--- src/app/person/persone/persone.component.ts
+++ src/app/person/persone/persone.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+
+import { RequestService } from 'src/app/services/RequestService';
+import { HttpErrorResponse } from '@angular/common/http';
+
+import {User} from 'src/app/models/User';
 
 @Component({
   selector: 'app-persone',
@@ -8,21 +12,47 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class PersoneComponent implements OnInit {
 
-  public username = "John Doe";
-  public profession = "Eng";
-  public ranking = "Expert Geo";
-  public email = "JohnDoe@gmail.com";
-  public phonenumber = "000000000";
-  public city = "Almada";
-  public country = "Portugal";
-  public numberP = 1111;
+  isUInfoError: boolean = false;
+  userI: User = new User();  
 
-  constructor( public translate: TranslateService,) { 
-    translate.addLangs(['pt','en']); 
-    translate.setDefaultLang(localStorage.getItem('language'));
+  constructor(private req: RequestService) { }
+
+  ngOnInit(): void {  }
+
+  getPersoneInfo(){
+    this.req.getUserInfo().subscribe(
+  
+      (data : any )=>{
+        
+        this.userI.user_username = data.user_username;
+        this.userI.user_name = data.user_name;
+        this.userI.user_email = data.user_email;
+        this.userI.user_role = data.user_role;
+        if(data.user_street == ""){this.userI.user_street = "-";}
+        else
+          this.userI.user_street = data.user_street;
+        if(data.user_place == ""){this.userI.user_place = "-";}
+        else
+          this.userI.user_place = data.user_place;
+        if(data.user_country == ""){this.userI.user_country = "-";}
+          else this.userI.user_country = data.user_country;
+      },
+
+      (err : HttpErrorResponse)=>{
+        this.isUInfoError = true;}
+
+    );
   }
 
-  ngOnInit(): void {
+  loadPersone(){
+    const st = localStorage.getItem("userInfo");
+    if(st==null){
+      this.getPersoneInfo();
+    }
+    else this.userI = JSON.parse(st);
   }
 
+  savePersone(){
+    localStorage.setItem("userInfo", JSON.stringify(this.userI));
+  }
 }
