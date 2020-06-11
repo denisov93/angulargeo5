74e76243823e22237cd7cc7fb3b205fd5e0be6ab
Sara Trafaria <sara.afonsoft@gmail.com>
2020-06-11 16:30:54 +0100

    Admin modules

---
+++ src/app/admin-modules/admin-management/admin-management.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-admin-management',
+  templateUrl: './admin-management.component.html',
+  styleUrls: ['./admin-management.component.scss']
+})
+export class AdminManagementComponent implements OnInit {
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  firstName = new FormControl('',[Validators.required]);
+  lastName = new FormControl('',[Validators.required]);
+  username = new FormControl('',[Validators.required]);
+  street = new FormControl('',[Validators.required]);
+  zipcode = new FormControl('',[Validators.required]);
+  place = new FormControl('',[Validators.required]);
+  city = new FormControl('',[Validators.required]);
+  country = new FormControl('',[Validators.required]);
+
+  date = new FormControl(new Date());
+  serializedDate = new FormControl((new Date()).toISOString());
+
+  selected = '';
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  getEmailErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  getFirstNErrorMessage() {
+    if(this.firstName.hasError('required')){
+      return 'You must enter a first name';
+    }
+    //return this.firstName.hasError('firstName') ? 'Not a valid First Name' : '';
+  }
+
+  getLastNErrorMessage() {
+    if(this.lastName.hasError('required')){
+      return 'You must enter a last name';
+    }
+    return this.lastName.hasError('lastName') ? 'Not a valid Last Name' : '';
+  }
+
+  getUNErrorMessage(){
+    if(this.username.hasError('required')){
+      return 'You must enter a username';
+    }
+    //return this.username.hasError('username') ? 'Not a valid username' : '';
+  }
+
+  getStreetErrorMessage(){
+    if(this.street.hasError('required')){
+      return 'You must enter a street';
+    }
+    return this.street.hasError('street') ? 'Not a valid street' : '';
+  }
+
+  getZipCErrorMessage(){
+    if(this.zipcode.hasError('required')){
+      return 'You must enter a zip-code';
+    }
+    return this.zipcode.hasError('zipcode') ? 'Not a valid zip-code' : '';
+  }
+
+  getPlaceErrorMessage(){
+    if(this.place.hasError('required')){
+      return 'You must enter a place';
+    }
+    return this.place.hasError('place') ? 'Not a valid place' : '';
+  }
+
+  getCityErrorMessage(){
+    if(this.city.hasError('required')){
+      return 'You must enter a city';
+    }
+    return this.place.hasError('city') ? 'Not a valid city' : '';
+  }
+
+  getCountryErrorMessage(){
+    if(this.country.hasError('required')){
+      return 'You must enter a country';
+    }
+    return this.country.hasError('country') ? 'Not a valid country' : '';
+
+  }
+}
---
+++ src/app/admin-modules/map-management/map-management.component.ts
@@ -0,0 +1,66 @@
+import {Component, ElementRef, ViewChild, Output, EventEmitter} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-map-management',
+  templateUrl: './map-management.component.html',
+  styleUrls: ['./map-management.component.css']
+})
+export class MapManagementComponent {
+
+  //Copy paste do Form do mapa...
+
+  @Output() newDirectionAdded: EventEmitter<boolean> = new EventEmitter();
+  @ViewChild('labelImport')
+  labelImport: ElementRef;
+
+  formImport: FormGroup;
+  fileToUpload: File = null;
+
+  constructor(private router:Router) {
+    this.formImport = new FormGroup({
+      importFile: new FormControl('', Validators.required)
+    });
+  }
+
+  ngOnInit(): void {
+  }
+  onFileChange(files: FileList) {
+    this.labelImport.nativeElement.innerText = Array.from(files)
+      .map(f => f.name)
+      .join(', ');
+    this.fileToUpload = files.item(0);
+  }
+
+
+  import(): void {
+    var ss = '';
+ 
+    const reader = new FileReader();
+    reader.onload = function(event) {
+      ss = event.target.result.toString();
+      var start = ss.lastIndexOf("<coordinates>");
+      var end = ss.lastIndexOf("</coordinates>");
+      var smore = ss.slice(start,end);
+      var startn = smore.indexOf("-");
+      var allmixup = smore.slice(startn);
+      var ordered = [];
+      
+      ordered = allmixup.split('\n');
+      var orderedWtoSpace = [];
+      ordered.forEach(element => { 
+          orderedWtoSpace.push(element.trim().split(","));
+      });
+      orderedWtoSpace.pop();
+      localStorage.setItem("mySpecialDir",JSON.stringify(orderedWtoSpace));
+      
+    };
+    
+    reader.readAsText(this.fileToUpload);
+    
+    this.newDirectionAdded.emit(true);
+       
+  }
+
+}
