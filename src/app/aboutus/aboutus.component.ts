3ab92528c35d6911fbbda7d91d62ba62b282455a
Sara Trafaria <sara.afonsoft@gmail.com>
2020-06-13 20:25:58 +0100

    AboutUs update

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core'
 
 @Component({
   selector: 'app-aboutus',
-  templateUrl: './aboutus.component.html',
-  styleUrls: ['./aboutus.component.css']
+  templateUrl: './cenas.html',
+  styleUrls: ['./aboutus.component.scss']
 })
 
 export class AboutusComponent implements OnInit {
 
-  constructor() { 
-  
+  l:string;
+
+  constructor(public translate: TranslateService) { 
+
+    this.l = localStorage.getItem('language');
+    if(this.l==null){
+      this.l='pt';
+    } 
+    this.translate.setDefaultLang(this.l);
   }
   ngOnInit() { }
 
