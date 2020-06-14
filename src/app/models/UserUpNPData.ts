a341da68ebda81ed784d588aa132ce5b23109703
Sara Trafaria <sara.afonsoft@gmail.com>
2020-06-14 21:45:20 +0100

    update user models

--- src/app/models/UserUpNPData.ts
+++ src/app/models/UserUpNPData.ts
@@ -1,7 +1,7 @@
 export class UserUpNPData{
-    userUp_name: String
-    userUp_email: String  
-    userUp_street: String
-    userUp_place: String
-    userUp_country: String
+    user_name: String
+    user_email: String  
+    user_street: String
+    user_place: String
+    user_country: String
 }
