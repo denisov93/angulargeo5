edc95aa0eea5b4e72ea266124dca376926adde45
Sara Trafaria <sara.afonsoft@gmail.com>
2020-06-14 20:36:03 +0100

    models update

---
+++ src/app/models/UserUpdateData.ts
@@ -0,0 +1,8 @@
+export class UserUpdateData{
+    userUp_name: String
+    userUp_email: String
+    userUp_password: String   
+    userUp_street: String
+    userUp_place: String
+    userUp_country: String
+}
