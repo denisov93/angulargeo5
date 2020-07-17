4a183b240b31ac08c9c8a0f0c4ddfb192587e8a1
Sara Trafaria <sara.afonsoft@gmail.com>
2020-07-17 16:34:49 +0100

    auth update

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,22 +2,34 @@ import { Injectable } from '@angular/core';
 import { CanActivate, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree ,Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import { RequestService } from '../services/RequestService';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanActivateChild {
-
-  constructor(private request:RequestService, private router:Router){}
+  helper;
+  constructor(private request:RequestService, private router:Router,public jwtHelper: JwtHelperService){
+    this.helper = new JwtHelperService(); 
+  }
 
   canActivate(){
     if(this.request.isLoggedIn()){
-      return true;
+
+      const decodedToken = this.helper.decodeToken(localStorage.getItem('tokenID'));
+      if(decodedToken.token.role==="SU"||decodedToken.token.role==="BOM"||decodedToken.token.role==="BOP"){
+        this.router.navigate(['/admin']);
+      }
+      else{        
+        return true;
+      }  
+      
     }
     else{
       this.router.navigate(['/signin']);
       return false;
     }
+
   }
   canActivateChild(
     next: ActivatedRouteSnapshot,
