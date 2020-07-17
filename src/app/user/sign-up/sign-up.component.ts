7be02034509fbce218b442d6447d7fa7a25f0c9d
Sara Trafaria <sara.afonsoft@gmail.com>
2020-07-17 16:41:11 +0100

    user update

--- src/app/user/sign-up/sign-up.component.ts
+++ src/app/user/sign-up/sign-up.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit ,NgModule} from '@angular/core';
-import { RequestService } from '../../services/RequestService';
+//import { Component, OnInit ,NgModule} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 
+import { RequestService } from '../../services/RequestService';
 
 @Component({
   selector: 'app-sign-up',
